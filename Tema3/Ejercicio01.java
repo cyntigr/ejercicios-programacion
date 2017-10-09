ba3792f7857f3b6a37ea88d6f66ebe82c2da8c5a
Cyntia Garcia Ruiz <cyntigr@gmail.com>
2017-10-09 09:48:40 +0200

    Actualización de ejercicios

---
+++ Tema3/Ejercicio01.java
@@ -0,0 +1,31 @@
+/**
+* Realiza un programa que pida dos números y que luego muestre el resultado
+de su multiplicación.
+*
+* @author Cyntia Garcia Ruiz
+*/
+
+public class Ejercicio01 {
+  public static void main(String[] args) {
+
+    String linea;
+
+    System.out.print("Por favor, introduce un número: ");
+    linea = System.console().readLine();
+    int primerNumero;
+    primerNumero = Integer.parseInt( linea );
+
+    System.out.print("introduce otro, por favor: ");
+    linea = System.console().readLine();
+    int segundoNumero;
+    segundoNumero = Integer.parseInt( linea );
+
+    int total;
+    total = primerNumero * segundoNumero;
+
+    System.out.print("El primer número introducido es " + primerNumero);
+    System.out.println(" y el segundo es " + segundoNumero);
+    System.out.print("La multiplicacion de los dos es ");
+    System.out.print(total);   
+  } 
+}
